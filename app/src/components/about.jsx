ee44d830750c6354183e61476e142e9a52d8e2cd
Hector <hi@hectorarce.es>
2016-07-22 00:39:57 +0200

    update all

---
+++ app/src/components/about.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, {Component} from "react";
+import home from "../../styles/index.scss";
+
+
+class About extends Component {
+
+  render() {
+    return <div>
+      <span>About, hello</span>
+    </div>
+
+  }
+
+}
+
+export default About;
---
+++ app/src/components/home.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import React, {Component} from "react";
+import home from "../../styles/index.scss";
+
+
+class Home extends Component {
+
+  render() {
+    return <div>
+      <span>what's up?</span>
+      <p><a href="/about">Go about</a></p>
+    </div>
+
+  }
+
+}
+
+export default Home;
